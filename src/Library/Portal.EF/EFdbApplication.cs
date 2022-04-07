8fe80732ddb25a203ea206ba73f463c078941662
Seyed Hassan Jabbekhaneh <jabbekhaneh@gmail.com>
2022-04-07 23:05:20 +0430

    user design

--- src/Library/Portal.EF/EFdbApplication.cs
+++ src/Library/Portal.EF/EFdbApplication.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Portal.Domain.Users;
 
-namespace Portal.EF
+namespace Portal.EF;
+public class EFdbApplication : DbContext
 {
-    internal class EFdbApplication
+    private readonly string _ConnectionString;
+    public EFdbApplication() : this("data source =.; initial catalog =_dbPortal; integrated security = True; MultipleActiveResultSets=True")
     {
+
     }
+
+    public EFdbApplication(string connectionString)
+    {
+        _ConnectionString = connectionString;
+    }
+
+    #region Users
+
+    public  DbSet<User>? Users { get; set; }
+    public DbSet<Role>? Roles { get; set; }
+    public DbSet<RoleUser>? UserRoles { get; set; }
+    public DbSet<Permission>? Permissions { get; set; }
+    public DbSet<RolePermission>? RolePermissions { get; set; }
+    public DbSet<UserLoginHistory>? UserLoginHistories { get; set; }
+
+    #endregion
+
+
 }
--- src/Library/Portal.EF/Users/Configs/UserConfig.cs
+++ src/Library/Portal.EF/Users/Configs/UserConfig.cs
@@ -1,13 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Portal.Domain.Users;
 
-namespace Portal.EF.Users.Configs
+namespace Portal.EF.Users.Configs;
+
+public class UserConfig : IEntityTypeConfiguration<User>
 {
-    public class UserConfig
+    public void Configure(EntityTypeBuilder<User> _)
     {
+        _.HasKey(_=> _.Id);
+        _.Property(_ => _.Id).IsRequired();
+        _.Property(_ => _.Mobile).IsRequired();
+        _.Property(_ => _.Firstname).HasMaxLength(255);
+        _.Property(_ => _.Lastname).HasMaxLength(255);
+        _.Property(_=>_.Address).HasMaxLength(1000);
+        _.Property(_ => _.Email);
+        _.Property(_=>_.City).HasMaxLength(255);
+        _.Property(_=>_.IsActiveMobile).IsRequired();
+        _.Property(_=>_.County).HasMaxLength(255);
+        _.Property(_ => _.Mobile).HasMaxLength(20);
+        _.Property(_=>_.Company).HasMaxLength(255);
+        _.Property(_ => _.Wallet).IsRequired();
+        _.Property(_ => _.ActiveCode).HasMaxLength(20);
+        _.Property(_ => _.DateOfBirth);
+        _.Property(_ => _.Education).HasMaxLength(255);
+        _.Property(_ => _.FatherName).HasMaxLength(255);
+        _.Property(_ => _.Token);
+        _.Property(_=>_.Geneder).IsRequired();
+        
+
 
     }
 }
